b960a40887a0d9e2eb524cab4a9610c97d2d3063
Scott Wolchok <swolchok@fb.com>
2022-03-29 20:33:11 -0700

    [PyTorch] Add & use in-place gelu
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74629
    
    It seems silly that we don't have this, so add it.
    ghstack-source-id: 152549926
    
    Differential Revision: [D35087899](https://our.internmc.facebook.com/intern/diff/D35087899/)

--- aten/src/ATen/native/cuda/Blas.cpp
+++ aten/src/ATen/native/cuda/Blas.cpp
@@ -299,7 +299,7 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
         at::relu_(const_cast<Tensor&>(*result_));
         break;
       case Activation::GELU:
-        result_ = c10::MaybeOwned<Tensor>::owned(at::gelu(*result_));
+        at::gelu_(const_cast<Tensor&>(*result_));
         break;
       default: break;
     }
@@ -307,7 +307,7 @@ Tensor& addmm_out_cuda_impl(Tensor& result, const Tensor& self, const Tensor& ma
 
 #if defined(CUDA_VERSION) && CUDA_VERSION >= 11000 && CUDA_VERSION < 11040 && !defined(_MSC_VER)
   if (useLtInterface && activation == Activation::GELU) {
-    result_ = c10::MaybeOwned<Tensor>::owned(at::gelu(*result_));
+    at::gelu_(const_cast<Tensor&>(*result_));
   }
 #endif
 
