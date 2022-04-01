3a7aee3c3bc0c9361bd85e6d890f494fe6f94431
wanchaol <wanchaol@devvm3348.frc0.facebook.com>
2022-04-01 13:47:15 -0700

    [shard] add ShardedTensor.cpu()
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74941
    
    Add ShardedTensor.cpu() API, we choose to add this first instead of `ShardedTensor.to()` bc the latter one is ambiguous and tricker (i.e. we need to figure out what's the meaning of ShardedTensor.to("cuda")).
    
    Note that when moving ShardedTensor to CPU from GPU, user need to explicitly pass in a new process_group that is compatible with the device, otherwise we will error out.
    ghstack-source-id: 152842150
    
    Differential Revision: [D35238133](https://our.internmc.facebook.com/intern/diff/D35238133/)

--- torch/distributed/_shard/sharded_tensor/api.py
+++ torch/distributed/_shard/sharded_tensor/api.py
@@ -8,6 +8,7 @@ from typing import (
     Sequence,
     Union
 )
+import copy
 import weakref
 
 import threading
@@ -312,6 +313,63 @@ class ShardedTensor(object):
                         )
 
                     out_narrow_view.copy_(tensor)
+
+    def cpu(
+        self,
+        memory_format=torch.preserve_format,
+        process_group=None
+    ) -> ShardedTensor:
+        """
+        Returns a copy of this object in CPU memory.
+
+        If this ShardedTensor is already on CPU memory, then no copy is
+        performed and original object is returned.
+
+        .. note:: When moving a ShardedTensor from GPU to CPU, the ShardedTensor might
+            need to be managed by a different type of ProcessGroup(i.e. ProcessGroupGloo),
+            it is the user's responsiblity to explicitly pass in a new process_group that
+            is compatible with CPU.
+        """
+        # TODO: make this a __torch_function__ op once ShardedTensor becomes a
+        # torch.Tensor subclass
+        if memory_format != torch.preserve_format and \
+                memory_format != torch.contiguous_format:
+            raise RuntimeError("Only `torch.contiguous_format` or "
+                               "`torch.preserve_format` is supported!")
+        all_on_cpu = True
+        for meta in self.metadata().shards_metadata:
+            if meta.placement.device().type != "cpu":
+                all_on_cpu = False
+                break
+
+        # if every shard is already on CPU, return the original object
+        if all_on_cpu:
+            return self
+
+        # if not, returns a copy of this object on CPU
+        list_shards: List[Shard] = []
+        # move all local shards to cpu, and change metadata
+        for shard in self._local_shards:
+            cpu_tensor = shard.tensor.cpu(memory_format=memory_format)
+            metadata = copy.deepcopy(shard.metadata)
+            metadata.placement._device = torch.device("cpu")
+            list_shards.append(
+                Shard(cpu_tensor, metadata)
+            )
+
+        st_meta = copy.deepcopy(self.metadata())
+        for meta in st_meta.shards_metadata:
+            if meta.placement.device().type != "cpu":
+                meta.placement._device = torch.device("cpu")
+
+        pg = self._process_group if process_group is None else process_group
+        st_cpu = ShardedTensor._init_from_local_shards_and_global_metadata(
+            list_shards,
+            sharded_tensor_metadata=st_meta,
+            process_group=pg,
+            init_rrefs=self._init_rrefs
+        )
+        return st_cpu
 
     @classmethod
     def _init_from_local_shards(
