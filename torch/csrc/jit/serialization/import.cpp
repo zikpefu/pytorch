2e2158df5cbc68694efd6cc4752923d455152577
Pavithran Ramachandran <pavithran@fb.com>
2022-04-01 22:47:05 -0700

    [PyTorchEdge] Backport from v9 flatbuffer to v8 pickle
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75026
    
    In this diff:
    1. Bump supported version to 9, which will serve as a placeholder for upcoming version bump to v9 for flatbuffer format migration.
    2. Implements backport from v9 flatbuffer file to v8 pickle file.
    ghstack-source-id: 152878938
    
    Differential Revision: [D34702597](https://our.internmc.facebook.com/intern/diff/D34702597/)

--- torch/csrc/jit/serialization/import.cpp
+++ torch/csrc/jit/serialization/import.cpp
@@ -398,7 +398,7 @@ Module load(
   switch (format) {
     case FileFormat::FlatbufferFileFormat: {
 #if defined(ENABLE_FLATBUFFER)
-      return load_jit_module_from_stream(in, device);
+      return load_jit_module_from_stream(in, extra_files, device);
 #else
       TORCH_CHECK(
           false, "Flatbuffer input file but the build hasn't enable flatbuffer")
@@ -429,7 +429,7 @@ Module load(
   switch (format) {
     case FileFormat::FlatbufferFileFormat: {
 #if defined(ENABLE_FLATBUFFER)
-      return load_jit_module_from_file(filename, device);
+      return load_jit_module_from_file(filename, extra_files, device);
 #else
       TORCH_CHECK(
           false, "Flatbuffer input file but the build hasn't enable flatbuffer")
